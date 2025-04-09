a5d483b42cbe465452ecf865515494d827b5a466
Grégory Renaud <ph62ldr@eduvaud.ch>
2025-04-09 15:19:50 +0200

    Added config file for sensitive content

---
+++ TPI-MED/Classes/Database.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using TPI_MED;
+
+public class Database
+{
+    private static string connectionString = Program.Configuration.GetConnectionString("Default");
+
+    public static MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+}
---
+++ TPI-MED/Classes/MailHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System;
+using Wisej.Web;
+using TPI_MED;
+using Microsoft.Extensions.Configuration;
+
+public static class MailHelper
+{
+    public static void EnvoyerMailValidation(string emailDestinataire, string token, string nom)
+    {
+        var lien = "https://dev.mediateur.mycpnv.ch/?token=" + token;
+
+        var mail = new MailMessage();
+        mail.To.Add(emailDestinataire);
+        mail.Subject = "Validation de votre compte Journal de médiation";
+
+        mail.Body = $@"
+                Bonjour {nom},
+
+                Merci pour votre inscription sur l'application Journal de médiation.
+
+                Veuillez cliquer sur le lien suivant pour valider votre compte :
+                {lien}
+
+                À bientôt,
+                L’équipe Journal de médiation
+                ";
+
+        string smtpHost = Program.Configuration["Smtp:Host"];
+        string smtpUser = Program.Configuration["Smtp:User"];
+        string smtpPass = Program.Configuration["Smtp:Password"];
+
+
+        mail.From = new MailAddress(smtpUser, "Validation de compte");
+
+        var smtp = new SmtpClient(smtpHost, 587);
+        smtp.Credentials = new NetworkCredential(smtpUser, smtpPass);
+        smtp.EnableSsl = true;
+
+        try
+        {
+            smtp.Send(mail);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Erreur lors de l'envoi du mail : " + ex.Message);
+        }
+    }
+}
