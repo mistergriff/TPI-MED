674a118a8395c1dbfdfce0ecf3096fbaa4217117
Grégory Renaud <ph62ldr@eduvaud.ch>
2025-04-29 08:53:22 +0200

    Update AccueilPage.cs

--- TPI-MED/Pages/AccueilPage.cs
+++ TPI-MED/Pages/AccueilPage.cs
@@ -1,21 +1,60 @@
-﻿using Wisej.Web;
+﻿using System;
+using Wisej.Web;
 
 
 public partial class AccueilPage : UserControl
 {
     public AccueilPage()
     {
-        InitializeComponent();
+        InitializeComponent();        
 
         this.Text = "Accueil";
 
+        var title = new Label()
+        {
+            Text = "Bienvenue sur l'application\nJournal de médiation !",
+            Dock = DockStyle.Fill,
+            TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        };
+        
         var label = new Label()
         {
-            Text = "Bienvenue sur l'accueil !",
+            Text = "Bonjour, nous somme le " + DateTime.Now.ToString("d"),
             Dock = DockStyle.Fill,
             TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         };
 
-        this.Controls.Add(label);
+        var dataButton = new Button()
+        {
+            Text = "Accès aux entrées",
+            Dock = DockStyle.Bottom,
+            Height = 50 
+        };
+
+        var statsButton = new Button()
+        {
+            Text = "Accès aux statistiques",
+            Dock = DockStyle.Bottom,
+            Height = 50 
+        };
+
+        this.Controls.AddRange(new Control[] {
+            title,
+            dataButton,
+            statsButton
+        });
+
+        dataButton.Click += DataButton_Click;
+        statsButton.Click += StatsButton_Click;
+    }
+
+    private void StatsButton_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+    private void DataButton_Click(object sender, EventArgs e)
+    {
+        
     }
 }
