41664598457f9cf5357221a6225b8f39a79b1bc6
Grégory Renaud <ph62ldr@eduvaud.ch>
2025-05-05 12:11:50 +0200

    Create EventAffichage.cs
    For ID selection (delete and update event)

---
+++ TPI-MED/Classes/EventAffichage.cs
@@ -0,0 +1,11 @@
+﻿public class EventAffichage
+{
+    public int Id { get; set; }
+    public string Date { get; set; }
+    public string Sujet { get; set; }
+    public string Personnes { get; set; }
+    public int Duree { get; set; }
+    public int TempsAdmin { get; set; }
+    public string Type { get; set; }
+    public string Motivations { get; set; }
+}
